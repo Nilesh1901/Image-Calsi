6a6058b999efc6e4956fa7e214a8efb638ef63d7
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-24 15:59:27 +0530

    init project

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-3xl text-red-500">helo</div>;
+}
+
+export default App;
