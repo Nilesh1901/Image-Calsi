bb3d696f7dd6bbd6d4e139fc065fa2e7ce3bdeae
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-24 17:14:36 +0530

    completed login page ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,46 +1,49 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
+import logo from "@/assets/login_image.svg";
 // import React, { useRef, useState } from "react";
 
 function Login() {
-  const handleLogin = (e) => {
-    e.preventDefault();
-  };
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  // };
 
   return (
-    <div className="h- w-full  bg-gradient-to-r from-emerald-400 to-cyan-400">
-      <div className="p-5 flex items-center justify-center">
+    <div className="h-screen w-full  bg-gradient-to-r from-emerald-400 to-cyan-400">
+      <div className="p-5 h-full flex items-center justify-center">
         <div className="flex lg:flex-row lg:h-[400px] flex-col rounded-md gap-10 shadow-lg">
           <img
-            className="sm:max-w-md lg:max-w-xl w-full object-cover rounded-md"
-            src="https://wallpaperbat.com/img/401988-minimalist-forest-wallpaper-top-free-minimalist-forest.jpg"
+            className="sm:max-w-md  w-full object-cover rounded-md"
+            src={logo}
             alt=""
           />
           <div className="md:w-[410px] mx-auto flex-2 w-full p-5">
             <h2 className="text-center font-semibold text-2xl mb-5">Login</h2>
-            <form action="" onSubmit={handleLogin}>
+            <form action="">
               <div className="mb-2">
                 <label htmlFor="username" className="font-semibold">
                   Username:
                 </label>
-                <input
+                <Input
                   required
                   placeholder="@username"
                   id="username"
                   type="text"
                   className="mt-1"
-                ></input>
+                />
               </div>
               <div>
                 <label htmlFor="password" className="font-semibold">
                   Password:
                 </label>
-                <input
+                <Input
                   placeholder="*******"
                   id="password"
                   type="password"
                   className="mt-1"
                   required
-                ></input>
+                />
               </div>
               <Button
                 type="submit"
@@ -51,9 +54,12 @@ function Login() {
             </form>
             <p className="text-xs sm:text-sm ">
               Don't have an account?
-              <span className="text-blue-500 ml-1 cursor-pointer">
+              <Link
+                to={"/signup"}
+                className="text-blue-500 ml-1 cursor-pointer"
+              >
                 Register
-              </span>
+              </Link>
             </p>
           </div>
         </div>
