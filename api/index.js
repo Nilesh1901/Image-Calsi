86286c10f893d3aa88af6e5355cb84c525bbd0ac
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-24 18:16:02 +0530

    added boilerplate code to index.js in api folder

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`app is running on port ${process.env.PORT}`)
+);
