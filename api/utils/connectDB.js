943ed2d7ea4aa45df616e474c31706f5d418abf5
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-25 01:03:32 +0530

    remove params from db connect function

--- api/utils/connectDB.js
+++ api/utils/connectDB.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 
 export async function ConnectDB() {
-  const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
   try {
-    await mongoose.connect(process.env.MONGODBCONNECTION, connectionParams);
+    await mongoose.connect(process.env.MONGODBCONNECTION);
     console.log("Db connected");
   } catch (error) {
     console.log(`${error.message} connection failed to DB`);
