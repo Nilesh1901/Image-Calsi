b664b205d1000f8fd5cfc7d7bf0c4f9214e28c4c
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-24 18:03:16 +0530

    added states to login and signup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,35 +2,39 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
 import logo from "@/assets/login_image.svg";
-// import React, { useRef, useState } from "react";
+import { useState } from "react";
 
 function Login() {
-  // const handleLogin = (e) => {
-  //   e.preventDefault();
-  // };
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
 
   return (
     <div className="h-screen w-full  bg-gradient-to-r from-emerald-400 to-cyan-400">
       <div className="p-5 h-full flex items-center justify-center">
         <div className="flex lg:flex-row lg:h-[22rem] flex-col rounded-md shadow-lg">
-          <img
-            className="object-cover rounded-md"
-            src={logo}
-            alt=""
-          />
+          <img className="object-cover rounded-md" src={logo} alt="" />
           <div className="md:w-[24rem] mx-auto  w-full p-5">
             <h2 className="text-center font-semibold text-2xl mb-5">Login</h2>
             <form action="">
               <div className="mb-2">
-                <label htmlFor="username" className="font-semibold">
-                  Username:
+                <label htmlFor="email" className="font-semibold">
+                  Email:
                 </label>
                 <Input
                   required
-                  placeholder="@username"
-                  id="username"
-                  type="text"
+                  placeholder="xyz@gmail.com"
+                  id="email"
+                  type="email"
+                  name="email"
                   className="mt-1"
+                  value={formData.email}
+                  onChange={handleOnChange}
                 />
               </div>
               <div>
@@ -38,11 +42,14 @@ function Login() {
                   Password:
                 </label>
                 <Input
+                  required
                   placeholder="*******"
                   id="password"
                   type="password"
                   className="mt-1"
-                  required
+                  name="password"
+                  value={formData.password}
+                  onChange={handleOnChange}
                 />
               </div>
               <Button
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -2,42 +2,57 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
 import logo from "@/assets/login_image.svg";
+import { useState } from "react";
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+
   function handleSignUp() {}
   return (
     <div className="h-screen w-full  bg-gradient-to-r from-emerald-400 to-cyan-400">
       <div className="p-5 h-full flex items-center justify-center">
-        <div className="flex lg:flex-row lg:h-[24rem] flex-col rounded-md shadow-lg">
-          <img
-            className="sm:max-w-sm  md:w-auto object-cover rounded-md"
-            src={logo}
-            alt=""
-          />
-          <div className="lg:w-[30rem] w-full flex flex-col justify-center p-4 sm:p-6">
-            <h2 className="text-center font-semibold text-2xl mb-5">Sign Up</h2>
+        <div className="flex lg:flex-row lg:h-[22rem] flex-col rounded-md shadow-lg">
+          <img className="object-cover rounded-md" src={logo} alt="" />
+          <div className="lg:w-[24rem] w-full flex flex-col  justify-center p-4 sm:p-6">
+            <h2 className="text-center font-semibold text-2xl mb-4">Sign Up</h2>
             <form action="" onSubmit={handleSignUp}>
-              <div className="mb-2 mt-2">
-                <label htmlFor="name" className="font-semibold">
-                  Name:
-                </label>
-                <Input
-                  placeholder="name"
-                  id="name"
-                  type="text"
-                  className="mt-1"
-                  required
-                />
-              </div>
-              <div className="mb-2">
+              <div className="">
                 <label htmlFor="username" className="font-semibold">
                   Username:
                 </label>
                 <Input
-                  placeholder="@username"
+                  placeholder="@Username"
                   id="username"
+                  name="username"
                   type="text"
-                  className="mt-1"
+                  className="mt-1 mb-2"
+                  value={formData.username}
+                  onChange={handleOnChange}
+                  required
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="email" className="font-semibold">
+                  Email:
+                </label>
+                <Input
+                  required
+                  placeholder="xyz@gmail.com"
+                  id="email"
+                  type="email"
+                  name="email"
+                  className="mt-1 mb-2"
+                  value={formData.email}
+                  onChange={handleOnChange}
                 />
               </div>
               <div>
@@ -45,21 +60,24 @@ function SignUp() {
                   Password:
                 </label>
                 <Input
+                  required
                   placeholder="*******"
                   id="password"
                   type="password"
                   className="mt-1"
-                  required
+                  name="password"
+                  value={formData.password}
+                  onChange={handleOnChange}
                 />
               </div>
               <Button
                 type="submit"
-                className="w-full my-5 bg-gradient-to-tr from-cyan-400 to-emerald-400 font-semibold"
+                className="w-full my-3 bg-gradient-to-tr from-cyan-400 to-emerald-400 font-semibold"
               >
                 Signup
               </Button>
             </form>
-            <p className="text-xs sm:text-sm mb-2">
+            <p className="text-xs sm:text-sm mb-3">
               Allready have an account?
               <Link to={"/login"} className="text-blue-500 ml-1 cursor-pointer">
                 Login
