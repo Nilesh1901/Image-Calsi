6787d7541ab4837a415446005e9032f10589830c
Nilesh1901 <knileshpatel.129@gmail.com>
2024-12-25 18:36:03 +0530

    added firebase for storing images

--- client/src/components/ui/UploadSection.tsx
+++ client/src/components/ui/UploadSection.tsx
@@ -32,37 +32,41 @@ function UploadSection() {
   };
   return (
     <div className=" mt-12">
-      <div className="flex flex-col items-center justify-center p-6">
-        <div
-          onDragOver={handleDragOver}
-          onDragLeave={handleDragLeave}
-          onDrop={handleDrop}
-          className={`w-96 mb-4 p-4 h-32 border-2 border-dashed ${
-            dragging ? "border-blue-500 bg-blue-50" : "border-gray-300"
-          } flex items-center justify-center text-zinc-300`}
-        >
-          {file ? (
-            <p>{file.name}</p>
-          ) : (
-            <p>Drag & Drop your file here or click to upload</p>
-          )}
-        </div>
-        <input
-          type="file"
-          onChange={handleFileChange}
-          className="mt-4"
-          style={{ display: "none" }}
-          id="fileInput"
-        />
-        <label
-          htmlFor="fileInput"
-          className="mt-2 mb-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
-        >
-          Choose File
-        </label>
-        <Button variant={"outline"} size={"lg"}>
-          Upload
-        </Button>
+      <div className="flex items-center justify-center p-6">
+        <form action="">
+          <div
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+            className={`w-96 mb-4 p-4 h-32 border-2 border-dashed ${
+              dragging ? "border-blue-500 bg-blue-50" : "border-gray-300"
+            } flex items-center justify-center text-zinc-300`}
+          >
+            {file ? (
+              <p>{file.name}</p>
+            ) : (
+              <p>Drag & Drop your file here or click to upload</p>
+            )}
+          </div>
+          <input
+            type="file"
+            onChange={handleFileChange}
+            className="mt-4"
+            style={{ display: "none" }}
+            id="fileInput"
+          />
+          <div className="flex items-center mt-2 justify-between">
+            <label
+              htmlFor="fileInput"
+              className=" px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
+            >
+              Choose File
+            </label>
+            <Button variant={"outline"} size={"lg"}>
+              Upload
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
---
+++ client/src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-e76d7.firebaseapp.com",
+  projectId: "mern-blog-e76d7",
+  storageBucket: "gs://mern-blog-e76d7.appspot.com/image-calsi",
+  messagingSenderId: "1068116792935",
+  appId: "1:1068116792935:web:477ffb4456e0f8578326c2",
+  measurementId: "G-B0R012PTN5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
